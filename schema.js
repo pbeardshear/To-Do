cd1feed5872ae86291146a0d5513e699a813b7ff
Peter Beardshear <pbeardshear@berkeley.edu>
2012-02-20 22:11:07 -0800

    added new task

--- schema.js
+++ schema.js
@@ -1,18 +1,35 @@
 /** Database Models */
-var User;
+var User,
+	Task;
 
-exports.defineModels = function defineModels( mongoose, cb ){
+exports.defineModels = function defineModels (mongoose, cb) {
     var Schema = mongoose.Schema;
+	var ObjectID = Schema.ObjectId;
+	
     User = new Schema({
 	    username: {
-		type: String,
-		index: {unique: true}
+			type: String,
+			index: { unique: true }
 	    },
 	    password: {
-		type: String,
-		index: {required:true}
-	    }});
-
-    mongoose.model('User',User);
-    if(cb)cb();
+			type: String,
+			index: { required: true }
+		}
+	});
+	
+	Task = new Schema({
+		owner: { type: ObjectID, ref: 'User' },
+		name: { type: String, index: { required: true } },
+		group: { type: String },
+		due: { type: Date, required: true },
+		estimate: { type: String },
+		autoUpdate: { type: Boolean }
+	});
+	
+    mongoose.model('User', User);
+	mongoose.model('Task', Task);
+    
+	if (cb) {
+		cb();
+	}
 };
--- server.js
+++ server.js
@@ -1,30 +1,78 @@
 var exp = require('express');
 var mongoose = require('mongoose');
 var schema = require('./schema.js');
+var mongoStore = require('connect-mongodb');
 
 /** Defining the models */
-mongoose.connect('mongodb://localhost/');
-schema.defineModels(mongoose, function(){
+var db = mongoose.connect('mongodb://localhost/todo');
+var User, Task;
+schema.defineModels(mongoose, function() {
 	User = mongoose.model('User');
-    });
+	Task = mongoose.model('Task');
+});
 
-var app = exp.createServer(exp.logger(), exp.bodyParser(), exp.static(__dirname + '/public'));
+function loadUser (req, res, next) {
+	res.local('header', '');
+	res.local('title', '<title>To Do</title>');
+	User.findById(req.session.userID, function (err, user) {
+		if (err) {
+			console.error(err);
+		}
+		if (user) {
+			req.currentUser = user;
+			req.isLoggedIn = true;
+			res.local('isLoggedIn', true);
+		} else {
+			res.local('isLoggedIn', false);
+		}
+		next();
+	});
+}
+
+var app = exp.createServer(exp.logger(), exp.bodyParser(), exp.static(__dirname + '/public'), exp.cookieParser(),
+			exp.session({ secret: 'secret', store: mongoStore(db) }), loadUser);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(request, response){
-	response.render('index');
-    });
+app.get('/', function(request, response) {
+	var tasks;
+	if (response.isLoggedIn) {
+		// Retrieve the list of tasks for this user
+		tasks = Task.find({ owner: response.currentUser._id });
+	}
+	response.render('index', { tasks: tasks });
+});
 
-app.post('/', function(request, response){
+app.post('/', function(request, response) {
 	response.send(request.body.data);
-    });
+});
 
-app.get('/:id', function(request, response){
+app.get('/newTask', function (request, response) {
+	response.render('newTask');
+});
+
+app.post('/newTask', function (request, response) {
+	// TODO: validation check
+	// Get the post data
+	var body = request.body;
+	(new Task({ owner: request.currentUser, name: body.name, due: body.dueDate, estimate: body.estimate, autoUpdate: body.autoUpdate })
+		.save(function (err, cost) {
+			if (err) {
+				console.error(err);
+				response.render('newTask', { error: err });
+			} else {
+				// Redirect to the main page to display the list of tasks
+				response.redirect('/');
+			}
+		})
+	);
+});
+
+app.get('/:id', function(request, response) {
 	response.send(request.params.id);
-    });
+});
 
 var port = process.env.PORT || 1337;
-app.listen(port, function(){
+app.listen(port, function() {
 	console.log("Server started");
-    });
+});
